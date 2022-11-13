d9256eb082db155765f1c0cdc35a4670beda712b
brittadam <britt.adam@hotmail.com>
2022-11-13 10:58:50 +0100

    Header

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.header}>
+      <Text style={styles.h1}>Bookshop</Text>
+      <View style={styles.cart}>
+        <Image style={styles.icon} source={require('./assets/cart.png')}/>
+        <Text style={styles.number}>1</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    display:'flex',
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'row',
+    marginTop: 60,
+    margin: 20,
   },
+  h1:{
+    fontSize: 40,
+   
+  },
+  icon:{
+    maxHeight:40,
+    width:40,
+    marginLeft:50,
+  },
+  number:{
+    fontSize: 20, 
+    marginTop: 20,
+  },
+  cart:{
+    display:'flex',
+    flexDirection:'row',
+  }
 });

610a6300f5e67e5b3d440fcdc4f7fb0d46d4183c
brittadam <britt.adam@hotmail.com>
2022-11-13 11:38:19 +0100

    book1

--- App.js
+++ App.js
@@ -3,25 +3,42 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.header}>
-      <Text style={styles.h1}>Bookshop</Text>
-      <View style={styles.cart}>
-        <Image style={styles.icon} source={require('./assets/cart.png')}/>
-        <Text style={styles.number}>1</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <StatusBar style="auto" />
+        <Text style={styles.h1}>Bookshop</Text>
+        <View style={styles.cart}>
+          <Image style={styles.icon} source={require('./assets/cart.png')}/>
+          <Text style={styles.number}>1</Text>
+        </View>
+        
       </View>
-      <StatusBar style="auto" />
+
+      <View style={styles.book}>
+          <Text style={styles.h2}>Book title 1</Text>
+          <Text style={styles.writer}>by Some Random dude</Text>
+          <Text style={styles.summary}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+          <View style={styles.extra}>
+            <Text style={styles.isbn}>ISBN 978-7-2165-1860-1</Text>
+            <Text style={styles.btn}>Read</Text>
+          </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  container:{
+    margin: 20,
+  },
   header: {
     display:'flex',
     justifyContent: 'space-between',
     backgroundColor: '#fff',
     flexDirection:'row',
     marginTop: 60,
-    margin: 20,
+    marginBottom:40,
   },
   h1:{
     fontSize: 40,
@@ -39,5 +56,27 @@ const styles = StyleSheet.create({
   cart:{
     display:'flex',
     flexDirection:'row',
+  },
+  h2:{
+    fontSize: 25,
+  },
+  writer:{
+    marginBottom: 20,
+  },
+  summary:{
+    marginBottom:15,
+  },
+  extra:{
+    display:'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between'
+  },
+  btn:{
+      borderWidth:2,
+      padding:5,
+  },
+  book:{
+    paddingBottom:10,
+    borderBottomWidth:2,
   }
 });
